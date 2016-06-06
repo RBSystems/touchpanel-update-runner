553ff8e8acc4b6abdd798272e2fb91838fd5cbbc
Jesse Millar <hellojessemillar@gmail.com>
2016-06-06 16:17:57 -0600

    Adding Beanstalk stuff

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
-# Pasted into Jenkins to build (will eventually be fleshed out to work with a Docker Hub and Amazon AWS)
 
-echo "Stopping running application"
-docker stop touchpanel-update-runner
-docker rm touchpanel-update-runner
+PROJECT_NAME=$1
+SHA1=$2 # Nab the SHA1 of the desired build from a command-line argument
+EB_BUCKET=elasticbeanstalk-us-west-2-194925301021
 
-echo "Building container"
-docker build -t byuoitav/touchpanel-update-runner .
+# Create new Elastic Beanstalk version
+DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
+sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+aws configure set default.region us-west-2
+aws configure set region us-west-2
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE # Copy the Dockerrun file to the S3 bucket
+aws elasticbeanstalk create-application-version --application-name $PROJECT_NAME --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
-echo "Starting the new version"
-docker run -d --restart=always --name touchpanel-update-runner -p 8003:8003 byuoitav/touchpanel-update-runner:latest
+# Update Elastic Beanstalk environment to new version
+aws elasticbeanstalk update-environment --environment-name $PROJECT_NAME-env --version-label $SHA1
